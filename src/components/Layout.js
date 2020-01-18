2e15466b9f39a7f442e743dbef23e7cf3651ae1e
Kazto TAKAHASHI <kazto@kazto.dev>
2020-01-18 17:24:49 +0900

    favicon追加

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -16,20 +16,20 @@ const TemplateWrapper = ({ children }) => {
         <meta name="description" content={description} />
 
         <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href={`${withPrefix('/')}img/apple-touch-icon.png`}
+          rel="stamp"
+          sizes="400x400"
+          href={`${withPrefix('/')}img/stamp.png`}
         />
         <link
           rel="icon"
           type="image/png"
-          href={`${withPrefix('/')}img/favicon-32x32.png`}
+          href={`${withPrefix('/')}img/stamp-32x32.png`}
           sizes="32x32"
         />
         <link
           rel="icon"
           type="image/png"
-          href={`${withPrefix('/')}img/favicon-16x16.png`}
+          href={`${withPrefix('/')}img/stamp-16x16.png`}
           sizes="16x16"
         />
 
